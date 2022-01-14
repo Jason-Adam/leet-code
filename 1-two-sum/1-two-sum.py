90e62f04c74c83c087f640df804dc8e58b3a6e5e
Jason Adam <35201417+Jason-Adam@users.noreply.github.com>
2022-01-13 22:35:48 -0700

    Time: 86 ms (52.57%), Space: 15.5 MB (19.22%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,11 +1,12 @@
 class Solution:
+    """O(n) time | O(n) space"""
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         pairs = {}
         
-        for idx in range(len(nums)):
-            delta = target - nums[idx]
+        for idx, num in enumerate(nums):
+            delta = target - num
             
             if delta in pairs:
                 return [pairs[delta], idx]
             
-            pairs[nums[idx]] = idx
+            pairs[num] = idx
